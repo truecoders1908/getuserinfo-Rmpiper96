a4b29065787b4468cce5aa278e1ec638e1823319
unknown <rmpiper96@gmail.com>
2019-09-06 08:33:52 -0500

    Still need to remember to make commits as I go

---
+++ ProjectImNotSureWhatItIS/User.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjectImNotSureWhatItIS
+{
+    class User
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string FavAnimal { get; set; }
+        public string FavColor { get; set; }
+        public int FavNumber { get; set; }
+        public int NumPets { get; set; }
+        public string petNames { get; set; }
+        public string AnswerQuestions(string question)
+        {
+            Console.WriteLine(question);
+            return Console.ReadLine();
+        }
+        public int AnswerQuestionWithIntiger(string question)
+        {
+            string stringAnswer = AnswerQuestions(question);
+            return int.Parse(stringAnswer);
+        }
+        public int AnswerQuestionWithIntiger2(string question)
+        {
+            string stringAnswer = AnswerQuestions(question);
+            int x = int.Parse(stringAnswer);
+            return int.Parse(stringAnswer);
+        }
+        public string AnswerwithPetNames(string question)
+        {
+            Console.WriteLine(question);
+            
+            return Console.ReadLine();
+        }
+    }
+}
